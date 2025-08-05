fb0ae5897f7ffb1e8e01207e288218e8a93bddb5
Pavel <newoluch@yandex.ru>
2025-08-06 00:25:18 +0200

    расширение логики. Добавление новых элементов

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,8 +1,19 @@
 package service
 
 import (
-	"fmt"
+	"errors"
 	"time"
+)
+
+var (
+	DefaultTimeLayout      = "2006-01-02 15:04"
+	ErrTimerNotStarted     = errors.New("the timer is not started")
+	ErrTimerAlreadyPaused  = errors.New("the timer is already paused")
+	ErrTimerAlreadyRunning = errors.New("the timer is already running")
+	ErrTimerNotPaused      = errors.New("the timer is not paused")
+	ErrCannotSaveSplit     = errors.New("unable to save time - timer paused")
+	ErrListValues          = errors.New("the list of values is empty")
+	ErrCountValnFunc       = errors.New("count of values in the function is incorrect")
 )
 
 type Stopwatch struct {
@@ -11,102 +22,109 @@ type Stopwatch struct {
 	startTime time.Time // время начала работы таймера
 
 	// paused
-	isPaused       bool          // момент постановки таймера на паузу
+	isPaused       bool          // флаг постановки таймера на паузу
 	pauseTime      time.Time     // время начала паузы
 	pausedDuration time.Duration // время, проведённое на паузе
 
-	split []Split
+	split []Split // структура для фиксации отрезков времени
 }
 
 type Split struct {
-	checkTime    time.Time // сохраняет промежуточное время
-	pausedBefore time.Duration
+	checkTime    time.Time     // сохраняет промежуточное время
+	pausedBefore time.Duration // время проведённое на паузе, до сохранения сплита
 }
 
-// запустить и сбросить секундомер
-func (s *Stopwatch) Start() {
+type Stat struct {
+	IsWorking     bool
+	IsPaused      bool
+	StartTime     time.Time
+	PausedTime    time.Time
+	Elapsed       time.Duration
+	SplitsCount   int
+	Splits        []time.Duration
+	AllpausedTime time.Duration
+}
+
+// запустить и сбросить таймер
+func (s *Stopwatch) Start() error {
 	if s.isWorking {
-
-		// сброс
-		s.startTime = time.Time{}
-		s.isWorking = false // выставляет статус работы таймера как "не запущен"
-		s.split = []Split{}
-
-		s.isPaused = false
-		s.pauseTime = time.Time{}
-		s.pausedDuration = 0
-		return
+		return ErrTimerAlreadyRunning
 	}
 
 	s.isWorking = true
 	s.startTime = time.Now()
-	s.split = []Split{}
+	s.split = nil
+	return nil
 }
 
-func (s *Stopwatch) Pause() {
+func (s *Stopwatch) Reset() {
+	s.isWorking = false
+	s.startTime = time.Time{}
+
+	s.isPaused = false
+	s.pauseTime = time.Time{}
+	s.pausedDuration = 0
+	s.split = nil
+}
+
+func (s *Stopwatch) Pause() error {
 	if !s.isWorking {
-		fmt.Println("Timer not started")
-		return
+		return ErrTimerNotStarted
 	}
 
 	if s.isPaused {
-		fmt.Println("the timer is already paused")
-		return
+		return ErrTimerAlreadyPaused
 	}
 
 	s.isPaused = true
 	s.pauseTime = time.Now()
+	return nil
 }
 
-func (s *Stopwatch) Resume() {
+func (s *Stopwatch) Resume() error {
 	if !s.isPaused {
-		fmt.Println("the timer is not paused")
-		return
+		return ErrTimerNotPaused
 	}
 
 	s.pausedDuration += time.Since(s.pauseTime)
 
-	fmt.Println("Timer started. Time spent on pause:", s.pausedDuration.Round(time.Second).String())
-
 	s.isPaused = false
+	return nil
 }
 
-// сохранить промежуточное время
-func (s *Stopwatch) SaveSplit() {
+// зафиксировать время работы таймера в данный момент (без учета паузы)
+func (s *Stopwatch) SaveSplit() error {
 
 	if !s.isWorking {
-		fmt.Println("the timer is not started")
-		return
+		return ErrTimerNotStarted
 	} else if s.isPaused {
-		fmt.Println("unable to save time - timer paused")
-		return
+		return ErrCannotSaveSplit
 	}
 
 	s.split = append(s.split, Split{checkTime: time.Now(), pausedBefore: s.pausedDuration})
-
+	return nil
 }
 
-// Возвращает время от startTime до текущего момента за вычетом pausedDuration
+// возвращает время от startTime до текущего момента за вычетом pausedDuration
 func (s *Stopwatch) Elapsed() time.Duration {
 
-	// Если таймер на паузе — считает до момента pauseTime
+	// если таймер на паузе — считает до момента pauseTime
 	if s.isPaused {
 
-		passed := s.pauseTime.Sub(s.startTime)
+		passed := s.pauseTime.Sub(s.startTime).Round(time.Second)
 		return passed
 	}
 
-	passed := time.Since(s.startTime)
+	passed := time.Since(s.startTime).Round(time.Second)
 	passed = passed - s.pausedDuration
 	return passed
 }
 
-// вернуть текущий результат
+// вернуть текущий результат из SaveSplit()
 func (s *Stopwatch) GetResults() []time.Duration {
 
 	if len(s.split) == 0 {
-		fmt.Println("Список значений пуст")
-		return []time.Duration{0}
+		return []time.Duration{}
 	}
 
 	var result []time.Duration
@@ -115,4 +133,47 @@ func (s *Stopwatch) GetResults() []time.Duration {
 		result = append(result, currValue.Round(time.Second))
 	}
 	return result
+}
+
+func (s *Stopwatch) GetSpentOnPause() time.Duration {
+	return s.pausedDuration.Round(time.Second)
+}
+
+func (s Stopwatch) GetStatistics() Stat {
+
+	getSplits := s.GetResults()
+
+	return Stat{
+		IsWorking:     s.isWorking,
+		IsPaused:      s.isPaused,
+		StartTime:     s.startTime,
+		PausedTime:    s.pauseTime,
+		Elapsed:       s.Elapsed(),
+		SplitsCount:   len(s.split),
+		Splits:        getSplits,
+		AllpausedTime: s.pausedDuration,
+	}
+
+}
+
+func (s *Stopwatch) GetTime(layout ...string) (string, error) {
+
+	// если задано больше одного аругмента
+	if len(layout) > 1 {
+		return "", ErrCountValnFunc
+	}
+
+	// если макет не задан используем дефолтный
+	timeLayout := DefaultTimeLayout
+
+	if len(layout) == 1 {
+		timeLayout = layout[0]
+	}
+
+	// если таймер не запущен
+	if s.startTime.Equal(time.Time{}) {
+		return "", ErrTimerNotStarted
+	}
+
+	return s.startTime.Format(timeLayout), nil
 }
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 
 	fmt.Println("Go")
+
 	var testTime service.Stopwatch
 	testTime.Start()
 	time.Sleep(3 * time.Second)
@@ -17,10 +18,11 @@ func main() {
 	testTime.Pause()
 	time.Sleep(4 * time.Second)
 	testTime.Resume()
+	time.Sleep(10 * time.Second)
 	testTime.SaveSplit()
 
-	r := testTime.GetResults()
-
+	r, _ := testTime.GetResults()
 	fmt.Println(r)
 
+	testTime.GetTime()
 }
