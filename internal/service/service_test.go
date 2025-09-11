fee57ad67605d23b4e5046479efb391c19ea04d5
Pavel <newoluch@yandex.ru>
2025-09-11 22:56:32 +0200

    new test added

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,10 +14,31 @@ func TestStopwatch_Start(t *testing.T) {
 	err := sw.Start()
 	assert.NoError(t, err, "Start() не должна возвращать ошибку при первом запуске")
 	assert.False(t, sw.startTime.IsZero(), "Stopwatch.startTime не должен быть нулевым при запуске")
-	assert.True(t, sw.isWorking, "Stopwatch.isWorking должен быть true при запуске")
+	assert.True(t, sw.isWorking, "Stopwatch.isWorking должен быть true после запуска")
 
 	// повторный запуск
 	err = sw.Start()
 
 	assert.Error(t, err, "sw.Start() должна возвращать ошибку ErrTimerAlreadyRunning при повторном запуске")
+}
+
+func TestStopwatch_Reset(t *testing.T) {
+	s := Stopwatch{}
+	s.Start()
+	time.Sleep(10 * time.Second)
+	s.SaveSplit()
+	time.Sleep(5 * time.Second)
+	s.SaveSplit()
+	s.Pause()
+	s.Resume()
+
+	s.Reset()
+
+	assert.False(t, s.isWorking, "Stopwatch.isWorking должен быть false после Reset()")
+	assert.True(t, s.startTime.IsZero(), "Время начала работы таймера должно быть = 0 после Reset()")
+
+	assert.False(t, s.isPaused, "Stopwatch.isPaused должен быть false после Reset()")
+	assert.True(t, s.pauseTime.IsZero(), "Не должно быть пауз при сброшенном таймере")
+	assert.Zero(t, s.pausedDuration, "Продолжительность паузы должна быть = 0 после Reset()")
+	assert.Nil(t, s.split, "После Reset() не должно быть никаких сохранённых промежутков времени")
 }
